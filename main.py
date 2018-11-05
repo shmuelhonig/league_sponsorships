ecb7f7d04c66dc4dec3583c69803a25048242a67
Shmuel Honig <shonig@gmail.com>
2018-11-05 17:57:25 -0500

    Add sqlalchemy and models imports to main.py and create session object to communicate with database

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Leagues
+
+# create session object to communicate with database
+engine = create_engine('sqlite:///leagues.db?check_same_thread=False')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 app = Flask(__name__)
 app.secret_key = "supersekrit"
+
 
 @app.route('/')
 def home():
