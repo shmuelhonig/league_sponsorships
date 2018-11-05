7a474e4410aeb4087b086e3ddcd0811a572fef42
Shmuel Honig <shonig@gmail.com>
2018-11-05 17:48:20 -0500

    Add leagues model with name, lat, long and price columns

--- models.py
+++ models.py
@@ -5,5 +5,15 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class Leagues(Base):
+    __tablename__ = 'leagues'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    latitude = Column(Float)
+    longitude = Column(Float)
+    price = Column(Integer)
+
+
 engine = create_engine('sqlite:///leagues.db')
 Base.metadata.create_all(engine)
