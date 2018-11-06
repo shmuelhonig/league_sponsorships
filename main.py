ed5899509d89a58c16a0d0a3f6b87f2d25583740
Shmuel Honig <shonig@gmail.com>
2018-11-05 22:54:01 -0500

    Add route to main.py for accepting arguments (location etc.) and extracting list of leagues within given radius

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Leagues
 
+from geopy.distance import geodesic
+
 # create session object to communicate with database
 engine = create_engine('sqlite:///leagues.db?check_same_thread=False')
 Base.metadata.bind = engine
@@ -30,3 +32,21 @@ def add_league():
         return redirect(url_for('add_league'))
     else:
         return render_template('addLeague.html')
+
+@app.route('/find-leagues/')
+def find_leagues():
+    # extract values of query arguments
+    location = request.args.get('location')
+    radius = request.args.get('radius')
+    budget = request.args.get('budget')
+    # convert location parameter to tuple
+    location_tuple = (location.split(',')[0], location.split(',')[1])
+    # extract list of leagues from database
+    leagues = session.query(Leagues).all()
+    # get list of all leagues within given radius
+    nearby_leagues = []
+    for league in leagues:
+        distance = geodesic((league.latitude, league.longitude), location_tuple).miles
+        if distance < radius:
+            nearby_leagues.append(league)
+    return render_template('findLeagues.html')
